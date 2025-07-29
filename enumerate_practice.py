53c19b841a58966179d7f7632d220192dbde788d
Andrew Sheppard <andrewsheppard8@gmail.com>
2025-07-28 18:57:40 -0700

    Updating enumerate practice

---
+++ enumerate_practice.py
@@ -0,0 +1,24 @@
+# Write a loop using enumerate that prints out only the even-indexed fruits (index 0, 2, 4, etc.) along with their index.
+
+# fruits = ["apple", "banana", "cherry", "date", "elderberry"]
+# for index,fruit in enumerate(fruits):
+#     if index % 2 == 0:
+        # print(fruit)
+
+
+# Write a loop using enumerate that prints a message like this only for the names at odd indexes (1, 3, 5, ...):
+# Index 1: Bob is at an odd position.
+# Index 3: Diana is at an odd position.
+
+# names = ["Alice", "Bob", "Charlie", "Diana", "Eve", "Frank"]
+
+# for index,name in enumerate(names):
+#     if index%2 == 1:
+#         print(f"{name} is at an odd position.")
+
+# You have a list of words
+# Use a for loop with enumerate() to build a single string that looks like this:
+# "0:zero | 1:one | 2:two | 3:three | 4:four"
+
+words = ["zero", "one", "two", "three", "four"]
+    
